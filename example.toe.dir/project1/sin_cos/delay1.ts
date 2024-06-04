9537de570038bf52ef634b6b015d33b9802e241a
gameTark <>
2024-06-05 06:34:06 +0900

    initial commit

---
+++ example.toe.dir/project1/sin_cos/delay1.ts
@@ -0,0 +1,26 @@
+65538
+1
+74
+73
+0
+0
+1
+1
+0
+74
+74
+0
+60
+chan1
+0
+{
+   rate = 60
+   start = 74
+   tracklength = 1
+   tracks = 1
+   {
+      name = chan1
+      data_rle = 0.304767 
+   }
+
+}
